9655a7885212f88a00558d1409481e98fcb2b2ca
bm16ton <bm16ton@gmail.com>
2021-12-29 21:58:21 -0500

    add includes for reboot to bootloader 1200bps

--- hw/bsp/portenta/family.mk
+++ hw/bsp/portenta/family.mk
@@ -45,6 +45,7 @@ SRC_C += \
 	$(ST_HAL_DRIVER)/Src/stm32$(ST_FAMILY)xx_hal_gpio.c \
 	$(ST_HAL_DRIVER)/Src/stm32$(ST_FAMILY)xx_hal_uart.c \
 	$(ST_HAL_DRIVER)/Src/stm32$(ST_FAMILY)xx_hal_pwr_ex.c \
+	$(ST_HAL_DRIVER)/Src/stm32$(ST_FAMILY)xx_hal_rtc_ex.c \
 	$(ST_HAL_DRIVER)/Src/stm32$(ST_FAMILY)xx_hal_i2c.c
 
 
